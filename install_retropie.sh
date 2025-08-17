b95829841083a50a8733cc4acf0ed996103f9a93
NickM719 <137313806+NickM719@users.noreply.github.com>
2025-08-17 11:20:19 -0400

    Add files via upload

---
+++ install_retropie.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git
+cd ~
+git clone https://github.com/RetroPie/RetroPie-Setup.git
+cd RetroPie-Setup
+chmod +x retropie_setup
+sudo ./retropie_setup.sh
+echo run again, then install bash tweet, run emulationsation to boot, or enable autostart. After, dump your roms in there selective areas.
+echo Thank you for using this installer by Nick-M.
